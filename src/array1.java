e122965f95920a93cf4bf30312bc5233850f7393
Anjali <anjalikumari972001@gmail.com>
2022-01-09 22:01:24 +0530

    Initial commit

---
+++ src/array1.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class array1 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        int size = input.nextInt();
+
+        //defining the array
+        int[] num = new int[size];
+
+        System.out.println("Enter the elements of an array: ");
+        //taking input of array
+        for(int i =0;i<size;i++)
+        {
+             num[i]= input.nextInt();
+        }
+
+        System.out.println("Enter the target element");
+        //target that we want to search
+        int x = input.nextInt();
+
+        int ans = ls(num,x);
+        System.out.println("Your ans is: "+ans);
+
+
+     //return -1 if element not found in the array.
+    }
+    static int ls(int[] num,int x)
+    {
+        if(num.length==0)
+        {
+            return -1;
+        }
+        for(int i =0;i<num.length;i++)
+        {
+            int element=num[i];
+            if(element==x)
+            {
+                return i;
+            }
+
+        }
+         return-1;
+    }
+}
